97e36ced01678ceea8f54043269ae77b1caced89
sujitha626 <sujithareddy1212@gmail.com>
2021-07-22 13:05:07 +0530

    docker file

--- src/main/java/com/example/customerservice/model/Customer.java
+++ src/main/java/com/example/customerservice/model/Customer.java
@@ -4,17 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name="customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
-    private String email;
+
+    public Customer(String name) {
+        this.name = name;
+    }
 }
